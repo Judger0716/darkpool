d1a4fe16f972a79c8f70ade5ae1ee83646e92b10
root <wanweitao>
2021-05-18 18:33:21 +0800

    update

---
+++ README.md
@@ -0,0 +1,48 @@
+# Hyperledger Fabric 开发框架
+
+## 介绍
+
+我们需要开发的内容都在 `commercial-paper` 目录下，其中 `papercontract` 是链码，`organization` 是两个组织的配置文件以及客户端文件。
+
+我们计划将所有的合约放在一个链码里面，虽然目前链码目录下还只有一个合约。链码目录下的 `install_chaincode.sh` 会自动安装和目录同名的链码到两个组织的 peer 节点上。
+
+`darkpool` 对应的是组织2，`token` 对应的是组织1。
+
+每个组织的目录下都有 `application`，我们主要需要参考其中的 `queryapp.js` 来了解以特定用户身份调用合约的方式，以及参考 `enrollUser.js` 来添加新用户。
+
+## 开发流程
+
+首先进入 `commercial-paper` 目录，执行：
+
+```shell
+./network-starter.sh
+```
+
+来创建一个 Hyperledger Fabric 集群。然后进入 `commercial-paper/papercontract` 目录，执行：
+
+```shell
+./install_chaincode.sh
+```
+
+来安装链码。然后进入两个组织的 `application` 目录下，首先执行：、
+
+```shell
+npm install
+```
+
+安装相关的依赖，再运行注册用户的代码：
+
+```shell
+node enrollUser.js
+```
+
+然后可以参考 `https://hyperledger-fabric.readthedocs.io/zh_CN/release-2.2/tutorial/commercial_paper.html` 上的方式运行样例程序调用合约。
+
+自己修改链码中的合约后，只需进入 `commercial-paper/papercontract` 目录，再次执行：
+
+```shell
+./install_chaincode.sh
+```
+
+就会自动安装新版本的链码。
+
--- commercial-paper/organization/darkpool/application/addToWallet.js
+++ commercial-paper/organization/darkpool/application/addToWallet.js
@@ -13,13 +13,15 @@ const path = require('path');
 
 const fixtures = path.resolve(__dirname, '../../../../test-network');
 
+const username = "balaji"
+
 async function main() {
 
     // Main try/catch block
     try {
 
         // A wallet stores a collection of identities
-        const wallet = await Wallets.newFileSystemWallet('../identity/user/balaji/wallet');
+        const wallet = await Wallets.newFileSystemWallet('../identity/user/' + username + '/wallet');
 
         // Identity to credentials to be stored in the wallet
         const credPath = path.join(fixtures, '/organizations/peerOrganizations/org1.example.com/users/User1@org1.example.com');
@@ -27,7 +29,7 @@ async function main() {
         const privateKey = fs.readFileSync(path.join(credPath, '/msp/keystore/priv_sk')).toString();
 
         // Load credentials into wallet
-        const identityLabel = 'balaji';
+        const identityLabel = username;
 
         const identity = {
             credentials: {
