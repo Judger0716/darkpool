a7d527cd2799b7faad716d997f3b51c9e085c048
Judger0716 <49678968+Judger0716@users.noreply.github.com>
2021-05-24 21:41:59 +0800

    Change mspID

--- darkpool_dev/userApp/enrollAdmin.js
+++ darkpool_dev/userApp/enrollAdmin.js
@@ -40,7 +40,7 @@ async function main() {
         certificate: enrollment.certificate,
         privateKey: enrollment.key.toBytes(),
       },
-      mspId: 'Org1MSP',
+      mspId: 'Org2MSP',
       type: 'X.509',
     };
     await wallet.put('admin', x509Identity);
