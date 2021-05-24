437c5ad7078d2b8150ba372e1b4d359ba88c8e96
Judger0716 <49678968+Judger0716@users.noreply.github.com>
2021-05-24 21:46:19 +0800

    udpate 20210524

--- darkpool_dev/userApp/server.js
+++ darkpool_dev/userApp/server.js
@@ -1,5 +1,14 @@
 'use strict';
 
+//global variable
+var transfer_list = [];
+
+// Requirements
+const fs = require('fs');
+const yaml = require('js-yaml');
+const { Wallets, Gateway } = require('fabric-network');
+const { exit } = require('process');
+
 // Express Framework
 var express = require('express');
 var app = express();
@@ -8,7 +17,6 @@ var bodyParser = require('body-parser');
 // Load HTML resources
 app.use('/public', express.static('public'));
 app.use(bodyParser.urlencoded({extended:true}));
-<<<<<<< HEAD
 app.use(bodyParser.json())
 app.engine('.html', require('ejs').renderFile);
 
@@ -16,12 +24,6 @@ app.engine('.html', require('ejs').renderFile);
 var registerUser = require('./RegisterUser');
 var loginUser = require('./LoginUser');
 var QueryToken = require('./queryToken');
-=======
-
-// Load other nodejs files
-var registerUser = require('./RegisterUser')
-var loginUser = require('./LoginUser')
->>>>>>> 259cf59027edffad0b20f338ed75820788a02973
  
 /* Route List */
 
@@ -42,11 +44,7 @@ app.post('/login', async function(req, res){
     }
     console.log(login_status)
     if(login_status=='LOG_SUC'){
-<<<<<<< HEAD
         res.render(__dirname + "/" + "public/main.html",{username: req.body.username});
-=======
-        res.send('LOGIN SUCCESSFULLY');
->>>>>>> 259cf59027edffad0b20f338ed75820788a02973
     }
     else if(login_status=='LOG_ERR'){
         res.send('LOGIN ERROR');
@@ -59,11 +57,7 @@ app.get('/register', function (req, res) {
 })
 
 app.post('/register', async function (req, res) {
-<<<<<<< HEAD
     var reg_status = await registerUser.RegUser(req.body.username);
-=======
-    var reg_status = await registerUser.RegUser(req.body.username)
->>>>>>> 259cf59027edffad0b20f338ed75820788a02973
     var response = {
         "status": reg_status,
     }
@@ -80,13 +74,13 @@ app.post('/register', async function (req, res) {
     else res.send('FAILED!')
 })
 
-<<<<<<< HEAD
 // Query Account Info
 app.post('/getinfo', async function (req, res){
-    await QueryToken.QueryBalance(req.body.username).then(balance => {
+    await QueryToken.QueryBalance(req.body.username).then(result => {
         console.log('Queryapp program complete.');
         res.json({
-            'balance': balance
+            'symbol': result.symbol,
+            'balance': result.balance
         });
     }).catch((e) => {
         console.log('Queryapp program exception.');
@@ -96,10 +90,55 @@ app.post('/getinfo', async function (req, res){
     });
 })
 
-=======
->>>>>>> 259cf59027edffad0b20f338ed75820788a02973
+// Query Transfer Info
+app.post('/gettransfer', async function (req ,res){
+    res.json({
+        'transfer_list': transfer_list
+    });
+})
+
+
 // SERVER LISTENING
-var server = app.listen(9000, function () {
+var server = app.listen(9000, async function () {
+
+    // Get history transfer record and listen on new transfer
+    const wallet = await Wallets.newFileSystemWallet(process.cwd() + '/wallet');
+    const gateway = new Gateway();
+    // Specify userName for network access
+    const userName = 'admin';
+    let connectionProfile = yaml.safeLoad(fs.readFileSync('../organization/darkpool/gateway/connection-org2.yaml', 'utf8'));
+    let connectionOptions = {
+        identity: userName,
+        wallet: wallet,
+        discovery: { enabled: true, asLocalhost: true }
+    };
+    console.log('Connect to Fabric gateway.');
+    await gateway.connect(connectionProfile, connectionOptions);
+    console.log('Use network channel: mychannel.');
+    const network = await gateway.getNetwork('mychannel');
+    const tokenContract = await network.getContract('tokenContract', 'Token');
+    await tokenContract.addContractListener((event)=>{
+        // convert into JSON
+        var evt = JSON.parse(event.payload);
+        /*
+        // Event Name
+        var event_name = event.eventName;
+        evt['event_name'] = event_name;
+        */
+        // From, neglect mint
+        if(evt['from']!='0x0'){
+            var fromuser_start = evt['from'].search('CN=')+3;
+            var fromuser_end = evt['from'].search('C=')-3;
+            evt['from'] = evt['from'].substring(fromuser_start,fromuser_end);
+        }
+        // To
+        var touser_start = evt['to'].search('CN=')+3;
+        var touser_end = evt['to'].search('C=')-3;
+        evt['to'] = evt['to'].substring(touser_start,touser_end);
+        // Value
+        transfer_list.push(evt);  // Add to global variable
+    }, {startBlock : 0});  // From genesis block
+
     //var host = server.address().address
     var port = server.address().port
     console.log("Web Application Address: http://localhost:%s", port)
