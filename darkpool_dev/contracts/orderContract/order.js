0c3d476080f5f92083138e5f7e43d7fc59219019
wanweitao <48124446+wanweitao@users.noreply.github.com>
2021-05-28 20:03:17 +0800

    Update order.js

--- darkpool_dev/contracts/orderContract/order.js
+++ darkpool_dev/contracts/orderContract/order.js
@@ -64,7 +64,7 @@ class Order extends Contract {
     // order structure.
     const newOrder = {
       order_id: oid,
-      create_time: ctx.stub.getTxTimestamp(),
+      create_time: ctx.stub.getDateTimestamp(),
       creator: creator,
       type: type,
       amount: null,
