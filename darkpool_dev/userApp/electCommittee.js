bfbc534758680bddb27aed061cb08d2973ce905a
root <root@vm45361.akkocloud.com>
2021-06-03 23:42:58 +0800

    Update 2021-06-03 23:42

--- darkpool_dev/userApp/electCommittee.js
+++ darkpool_dev/userApp/electCommittee.js
@@ -1,19 +1,3 @@
-/*
- * Copyright IBM Corp. All Rights Reserved.
- *
- * SPDX-License-Identifier: Apache-2.0
-*/
-
-/*
- * This application has 6 basic steps:
- * 1. Select an identity from a wallet
- * 2. Connect to network gateway
- * 3. Access PaperNet network
- * 4. Construct request to query the ledger
- * 5. Evaluate transactions (queries)
- * 6. Process responses
- */
-
 'use strict';
 
 // Bring key classes into scope, most importantly Fabric SDK network class
@@ -22,12 +6,8 @@ const yaml = require('js-yaml');
 const { Wallets, Gateway } = require('fabric-network');
 const { exit } = require('process');
 
-var argv = process.argv.splice(2);
-
-const username = argv[0];
-
 // Main program function
-async function main() {
+exports.electCommittee = async function (username, amount){
 
     // A wallet stores a collection of identities for use
     const wallet = await Wallets.newFileSystemWallet(process.cwd() + '/wallet');
@@ -64,7 +44,7 @@ async function main() {
         const network = await gateway.getNetwork('mychannel');
 
         // Get addressability to commercial paper contract
-        console.log('Use Token smart contract.');
+        console.log('Use Committee smart contract.');
 
         const contract = await network.getContract('committeeContract', 'Committee');
 
@@ -72,102 +52,20 @@ async function main() {
         console.log('-----------------------------------------------------------------------------------------');
         console.log('****** Submitting Committee queries ****** \n\n ');
 
-        /*
-                let queryResponse = await contract.submitTransaction('SetOption', 'Dogecoin', 'DOGE', '6');
-                console.log(queryResponse.toString());
-                console.log('\n  SetOption query complete.');
-                console.log('-----------------------------------------------------------------------------------------\n\n');
-        */
-
-        let queryResponse = await contract.evaluateTransaction('GetCandidates');
-        console.log(queryResponse.toString());
-        console.log('\n  GetCandidates query complete.');
-        console.log('-----------------------------------------------------------------------------------------\n\n');
-
-        queryResponse = await contract.submitTransaction('Apply', '50');
+        let queryResponse = await contract.submitTransaction('Apply', amount.toString());
         console.log(queryResponse.toString());
         console.log('\n  apply query complete.');
         console.log('-----------------------------------------------------------------------------------------\n\n');
 
-        queryResponse = await contract.evaluateTransaction('GetCandidates');
-        console.log(queryResponse.toString());
         console.log('\n  GetCandidates query complete.');
         console.log('-----------------------------------------------------------------------------------------\n\n');
+        return true;
 
-
-        /*
-           let queryResponse3 = await contract.evaluateTransaction('ClientAccountID');
-           console.log(queryResponse3.toString());
-           console.log('\n  ClientAccountID query complete.');
-           console.log('-----------------------------------------------------------------------------------------\n\n');
-   
-      
-   
-           // 1 asset history
-           console.log('1. Query Commercial Paper History....');
-           console.log('-----------------------------------------------------------------------------------------\n');
-           let queryResponse = await contract.evaluateTransaction('queryHistory', 'MagnetoCorp', '00001');
-   
-           let json = JSON.parse(queryResponse.toString());
-           console.log(json);
-           console.log('\n\n');
-           console.log('\n  History query complete.');
-           console.log('-----------------------------------------------------------------------------------------\n\n');
-   
-           // 2 ownership query
-           console.log('2. Query Commercial Paper Ownership.... Papers owned by MagnetoCorp');
-           console.log('-----------------------------------------------------------------------------------------\n');
-           let queryResponse2 = await contract.evaluateTransaction('queryOwner', 'MagnetoCorp');
-           json = JSON.parse(queryResponse2.toString());
-           console.log(json);
-   
-           console.log('\n\n');
-           console.log('\n  Paper Ownership query complete.');
-           console.log('-----------------------------------------------------------------------------------------\n\n');
-   
-           // 3 partial key query
-           console.log('3. Query Commercial Paper Partial Key.... Papers in org.papernet.papers namespace and prefixed MagnetoCorp');
-           console.log('-----------------------------------------------------------------------------------------\n');
-           let queryResponse3 = await contract.evaluateTransaction('queryPartial', 'MagnetoCorp');
-   
-           json = JSON.parse(queryResponse3.toString());
-           console.log(json);
-           console.log('\n\n');
-   
-           console.log('\n  Partial Key query complete.');
-           console.log('-----------------------------------------------------------------------------------------\n\n');
-   
-   
-           // 4 Named query - all redeemed papers
-           console.log('4. Named Query: ... All papers in org.papernet.papers that are in current state of redeemed');
-           console.log('-----------------------------------------------------------------------------------------\n');
-           let queryResponse4 = await contract.evaluateTransaction('queryNamed', 'redeemed');
-   
-           json = JSON.parse(queryResponse4.toString());
-           console.log(json);
-           console.log('\n\n');
-   
-           console.log('\n  Named query "redeemed" complete.');
-           console.log('-----------------------------------------------------------------------------------------\n\n');
-   
-   
-           // 5 named query - by value
-           console.log('5. Named Query:.... All papers in org.papernet.papers with faceValue > 4000000');
-           console.log('-----------------------------------------------------------------------------------------\n');
-           let queryResponse5 = await contract.evaluateTransaction('queryNamed', 'value');
-   
-           json = JSON.parse(queryResponse5.toString());
-           console.log(json);
-           console.log('\n\n');
-   
-           console.log('\n  Named query by "value" complete.');
-           console.log('-----------------------------------------------------------------------------------------\n\n');
-   
-           */
     } catch (error) {
 
         console.log(`Error processing transaction. ${error}`);
         console.log(error.stack);
+        return false;
 
     } finally {
 
@@ -177,15 +75,3 @@ async function main() {
 
     }
 }
-main().then(() => {
-
-    console.log('Queryapp program complete.');
-
-}).catch((e) => {
-
-    console.log('Queryapp program exception.');
-    console.log(e);
-    console.log(e.stack);
-    process.exit(-1);
-
-});
