920fcb19b491f128b8f9b29b6f49cf7ff81c6655
root <1207146524@qq.com>
2021-07-20 11:30:12 +0800

    Update DealedOrderQuery

--- darkpool_dev/userApp/queryDealedOrders.js
+++ darkpool_dev/userApp/queryDealedOrders.js
@@ -59,7 +59,41 @@ exports.queryDealedOrder = async function (username) {
         // Extract Userful Infomation from raw JSON data
         var index = 0;
         while(queryResult[index]!=undefined){
-            DealedOrderList.push(queryResult[index]['Record']);
+            var deal_id = queryResult[index]['Record']['deal_id'];
+            var related_comm = [];
+            for(var i=0; i<queryResult[index]['Record']['context']['content'].length; i++){
+                related_comm.push(queryResult[index]['Record']['context']['content'][i]['name']);
+            }
+            var deal_price = queryResult[index]['Record']['context']['price'];
+            var deal_time;
+            for(var i=0;i<2;i++){
+                var user_start = queryResult[index]['Record']['order'][i]['creator'].search('CN=')+3;
+                var user_end = queryResult[index]['Record']['order'][i]['creator'].search('C=')-3;
+                queryResult[index]['Record']['order'][i]['creator'] = queryResult[index]['Record']['order'][i]['creator'].substring(user_start,user_end);
+                var date = new Date();
+                date.setTime(parseInt(queryResult[index]['Record']['order'][i]['create_time']['seconds'])*1000);
+                queryResult[index]['Record']['order'][i]['create_time'] = date.toUTCString();
+                date.setTime(parseInt(queryResult[index]['Record']['order'][i]['deal_time']['seconds'])*1000);
+                queryResult[index]['Record']['order'][i]['deal_time'] = date.toUTCString();
+                deal_time = date.toUTCString();
+                queryResult[index]['Record']['order'][i]['deal'] = queryResult[index]['Record']['order'][i]['deal'].toString();
+                queryResult[index]['Record']['order'][i]['share_info_visible'] = false;
+            }
+            var buyer = queryResult[index]['Record']['order'][0]['creator'];
+            var seller = queryResult[index]['Record']['order'][1]['creator'];
+            var buy_order = queryResult[index]['Record']['order'][0];
+            var sell_order = queryResult[index]['Record']['order'][1];
+            DealedOrderList.push({
+                'deal_id': deal_id,
+                'buyer': buyer,
+                'seller': seller,
+                'related_comm': related_comm,
+                'deal_price': deal_price,
+                'deal_time': deal_time,
+                'buy_order': buy_order,
+                'sell_order': sell_order,
+                'report_visible': false,
+            });
             index += 1
         }
         
